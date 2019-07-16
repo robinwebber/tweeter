e949086dc94b6de673865927bc11a87d8b7602c8
robinwebber <robinewebber@gmail.com>
2019-07-16 21:24:30 +0000

    character counter now functioning

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  
+  $(".new-tweet-form textarea").keyup(function() {
+    let maxCharacters = 140
+    const tweetLength = $(this).val().length
+    const remainingCharacters = maxCharacters - tweetLength;
+    //console.log(remainingCharacters);
+    $('#character-counter').text(remainingCharacters);
+
+    if (remainingCharacters < 0) {
+      $(this).siblings('span').addClass("has-error");
+    } else {
+      if ($(this).siblings('span').hasClass("has-error")) {
+        $(this).siblings('span').removeClass("has-error")
+      };  
+    }
+  })
+});
